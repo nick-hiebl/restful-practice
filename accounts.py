125c3d8bce827f24a7434ea8db83307637f02794
nick-hiebl <n.hiebl@student.unsw.edu.au>
2018-07-08 20:23:03 +1000

    Added initial content

---
+++ accounts.py
@@ -0,0 +1,68 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse, abort
+import sqlite3
+
+class Account:
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+
+    def get_username(self):
+        return self.username
+
+    def get_password(self):
+        return self.password
+    
+    def get_email(self):
+        return self.email
+
+    def __str__(self):
+        return "Account('{}', '{}', '{}')".format(self.username, self.password, self.email)
+
+    def safe_view(self):
+        return {
+            "username": self.username,
+            "email": self.email
+        }
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+	def get(self):
+		return {'hello': 'world'}
+
+accounts = []
+
+parser = reqparse.RequestParser()
+parser.add_argument('username')
+parser.add_argument('password')
+parser.add_argument('email')
+
+
+class Register(Resource):
+    def post(self):
+        args = parser.parse_args()
+        username = args['username']
+        password = args['password']
+        email = args['email']
+
+        new_account = Account(username, password, email)
+        print(new_account)
+
+        accounts.append(new_account)
+
+class AccountShower(Resource):
+    def get(self):
+        return {"accounts": [
+            a.safe_view() for a in accounts
+        ]}
+
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Register, '/register')
+api.add_resource(AccountShower, '/accounts')
+
+if __name__ == '__main__':
+	app.run(debug=True)
